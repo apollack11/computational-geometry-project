9d171f9faccf75976e990c4b7e03232a067a02d9
Gerik Zatorski <gerikzatorski@gmail.com>
2016-12-10 23:34:01 -0600

    algorithm file taking shape

--- Algorithm.py
+++ Algorithm.py
@@ -10,42 +10,77 @@ MAX_MOVE = 10
 # Enum like structures
 class GameState:
     P_TURN, E_TURN, GAME_OVER = range(3)
+class AlgoState:
+    K_PART = 0  # during construction of k-blocks
+    SWEEP = 1   # sweeping block graph until the evader is trapped in an extended block B+
+    CAPTURE = 2 # find and capture evader in B+
 
 class Game:
     """ Controls the state of the BattleGround through turn based interaction """
     def __init__(self):
-        self.bg = BattleGround()
-        self.view = View()
-        self.state == GameState.E_TURN
+        self.bg = BattleGround()        # model
+        self.view = View()              # view
+        self.state == GameState.E_TURN  # state of game
+        self.kpart = []                 # to calculate later
     
     def Start():
         while not self.state == GameState.GAME_OVER:
             self.UpdateView()
             # evader's turn
             if self.state == GameState.E_TURN:
+                # make random movement
                 self.state = GameState.P_TURN
             # pursuers' turn
             if self.state == GameState.P_TURN:
+                if AlgoState.K_PART:
+                    
+                if AlgoState.SWEEP:
+                    for p in self.bg.pursuers:
+                        view = Line2D(p.pose self.bg.evader.pos)
+                        if 
+                if AlgoState.CAPTURE:
+                    pass
                 self.state = GameState.E_TURN
 
     def UpdateView(self):
         self.view.PrintBoard(self.bg)
 
+    def IsCriticalMove(self):
+
 class BattleGround:
     """ Represents the state of the room in terms of the algorithm """
     def __init__(self, polygon):
-        self.speed = MAX_MOVE
-        self.P = polygon
+        self.speed = MAX_MOVE  #
+        self.P = polygon       #
         self.pursuers = []
-        self.evader = []
-        
+        self.evader = []       #
+        self.Be                # partition containing evader - TODO maybe: move this to pursuers
+
+    def AddPursuer(self, x, y):
+        if len(self.pursuers) == 0:
+            self.pursuers = np.array([x, y])
+        else:
+            np.append(self.pursuers, [x, y])
+
+    '''
+    def MovePursuers(self):
+        for p in self.pursuers:
+            p.
+    def MoveEvader(self):
+    ''' 
+
 class View:
     """ The view of the board """
     @staticmethod
     def PrintBoard(board):
         print board
 
-
+class Pursuer:
+    def __init__(self, x, y, heading):
+        self.x = x
+        self.y = y
+        self.heading = heading
+        
 ######################################### Testing #########################################
 # TODO-optional: use regex's here to figure out form of each line (esp color vs pt)
 def parse_polygons(lines):
